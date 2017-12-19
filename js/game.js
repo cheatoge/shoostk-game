0583de20dd2709265026ed2e6309629dee9ee4e0
Łukasz Szpil <cheatoge@gmail.com>
2017-12-20 00:51:11 +0100

    Game works lol

--- js/game.js
+++ js/game.js
@@ -1,42 +1,46 @@
-$(document).ready( () => {
+$(document).ready(() => {
 
   const gridSize = 4;
   const cellSize = 110;
   const cellSpacing = 10;   // Space between tiles
   const gridContainerPadding = 10;   // Grid container's padding
 
-  function randomizeInt(min, max){
+  function randomizeInt(min, max) {
     max++;
     return Math.floor(Math.random() * (max - min) + min);
   }
 
   class GridCell {
-    constructor(positionX, positionY){
-      this.position = { x: positionX, y: positionY};
-      this.htmlId = `x${positionX}y${positionY}`;
-      const cellProperties = GridCell._generateCellProperties();
+    constructor(positionX, positionY) {
+      this.position = {x: positionX, y: positionY};
+      this.postitionAsText = `x${positionX}y${positionY}`;
+      const cellProperties = GridCell.generateCellProperties();
       this.actionType = cellProperties.type;
       this.value = cellProperties.value;
     }
 
-    static _updateCellProperties() {
-      const cellProperties = GridCell._generateCellProperties();
-      this.actionType = cellProperties.type;
-      this.value = cellProperties.value;
+    appendToHtml() {
+      $('.grid-container').append(`<div class="grid-cell" id="${this.postitionAsText}">${this.actionType}${this.value}</div>`);
     }
 
-    static _generateCellProperties() {
+    updateCellProperties() {
+      const cellProperties = GridCell.generateCellProperties();
+      this.actionType = cellProperties.type;
+      this.value = cellProperties.value;
+      $(`#${this.postitionAsText}`).html(`${this.actionType}${this.value}`);
+    }
+
+    static generateCellProperties() {
       const val = randomizeInt(1, 4);
-      switch (val){
+      switch (val) {
         case 1:
-          return { type:'+', value: randomizeInt(1, 10)};
+          return {type: '+', value: randomizeInt(1, 10)};
         case 2:
-          return { type:'-', value: randomizeInt(1,10)};
+          return {type: '-', value: randomizeInt(1, 10)};
         case 3:
-          return { type:'*', value: 2};
+          return {type: '*', value: 2};
         case 4:
-          return { type:'/', value: randomizeInt(2,5)};
-
+          return {type: '/', value: randomizeInt(2, 5)};
       }
     }
   }
@@ -44,9 +48,9 @@ $(document).ready( () => {
   function create2DGrid(size) {
     let gameGrid = new Array(size);
     for (let x = 0; x < size; x++) {
-      gameGrid[size-1-x] = new Array(size);
-      for(let y = 0; y < size; y++) {
-        gameGrid[size-1-x][y] = new GridCell(y, x);
+      gameGrid[x] = new Array(size);
+      for (let y = 0; y < size; y++) {
+        gameGrid[x][y] = new GridCell(y, x);
       }
     }
     return gameGrid;
@@ -54,68 +58,94 @@ $(document).ready( () => {
 
   function createHtmlCells(grid) {
     for (let x = 0; x < gridSize; x++) {
-      for(const cell of grid[x]){
-        $('.grid-container').append(`<div class="grid-cell" id="${cell.htmlId}">${cell.actionType}${cell.value}</div>`);
+      for (const cell of grid[x]) {
+        cell.appendToHtml();
       }
     }
   }
 
+  const gameGrid = create2DGrid(gridSize);
+
   class Player {
-    constructor(){
-      this.position = { x: 0, y: 0};
-      this.htmlId = 'player';
+    constructor() {
+      this.position = {x: 0, y: 0};
+      this.playerID = 'player';
       this.moveRadius = cellSize + cellSpacing;
       this.value = 66;
       this.create();
     }
 
     create() {
-      $('.grid-container').append(`<div id="${this.htmlId}" class="player">${this.value}</div>`);
+      $('.grid-container').append(`<div id="${this.playerID}" class="player">${this.value}</div>`);
     }
 
     _updatePosition() {
-      const posX = `${String( this.position.x * this.moveRadius + gridContainerPadding )}px`; // calculate position and add margin
-      const posY = `${String( this.position.y * this.moveRadius + gridContainerPadding )}px`;
-      $(`#${this.htmlId}`).css({left: posX, bottom: posY});
+      const posX = `${String(this.position.x * this.moveRadius + gridContainerPadding)}px`; // calculate position and add margin
+      const posY = `${String(this.position.y * this.moveRadius + gridContainerPadding)}px`;
+      $(`#${this.playerID}`).css({left: posX, bottom: posY});
     };
 
     _isMovePossible(nextPosition) {
       const x = nextPosition.x;
       const y = nextPosition.y;
-      const gridContains = (position) => position <= gridSize-1 && position >= 0;
-      return ( gridContains(x) && gridContains(y) );
+      const gridContains = (position) => position <= gridSize - 1 && position >= 0;
+      return (gridContains(x) && gridContains(y));
+    }
+
+    _updateHtmlValue() {
+      $(`#${this.playerID}`).html(`${this.value}`);
     }
 
     _handlePlayerAction(nextPosition) {
-      if(this._isMovePossible(nextPosition)){
+      if (this._isMovePossible(nextPosition)) {
+        const steppedCell = gameGrid[gridSize - 1 - nextPosition.y][nextPosition.x];
         this.position = nextPosition;
         this._updatePosition();
+        switch (steppedCell.actionType) {
+          case '+':
+            this.value = Math.round(this.value += steppedCell.value);
+            break;
+          case '-':
+            this.value = Math.round(this.value -= steppedCell.value);
+            break;
+          case '*':
+            this.value = Math.round(this.value *= steppedCell.value);
+            break;
+          case '/':
+            this.value = Math.round(this.value /= steppedCell.value);
+            break;
+        }
+        steppedCell.updateCellProperties();
+        this._updateHtmlValue();
+        if (this.value === 6) {
+          alert('You won m8!');
+        }
       }
     }
 
     moveUp() {
-      let newPosition = { x: player1.position.x, y: player1.position.y+1 };
-      this._handlePlayerAction(newPosition);
+      const nextPosition = {x: player1.position.x, y: player1.position.y + 1};
+      this._handlePlayerAction(nextPosition);
     }
 
     moveDown() {
-      let newPosition = { x: player1.position.x, y: player1.position.y-1 };
-      this._handlePlayerAction(newPosition);
+      const nextPosition = {x: player1.position.x, y: player1.position.y - 1};
+      this._handlePlayerAction(nextPosition);
     }
 
     moveRight() {
-      let newPosition = { x: player1.position.x+1, y: player1.position.y };
-      this._handlePlayerAction(newPosition);
+      const nextPosition = {x: player1.position.x + 1, y: player1.position.y};
+      this._handlePlayerAction(nextPosition);
     }
 
     moveLeft() {
-      let newPosition = { x: player1.position.x-1, y: player1.position.y };
-      this._handlePlayerAction(newPosition);
+      const nextPosition = {x: player1.position.x - 1, y: player1.position.y};
+      this._handlePlayerAction(nextPosition);
     }
   }
 
   document.addEventListener('keydown', (e) => {
-    switch (e.code){
+    switch (e.code) {
       case 'KeyW':
       case 'ArrowUp':
         e.preventDefault();
@@ -138,8 +168,6 @@ $(document).ready( () => {
         break;
     }
   });
-
-  const gameGrid = create2DGrid(gridSize);
   createHtmlCells(gameGrid);
   const player1 = new Player();
 
